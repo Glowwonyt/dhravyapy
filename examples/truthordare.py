82593694b98ef49c0c612586fdd45076c179668b
Gloww <91305530+Glowwonyt@users.noreply.github.com>
2022-03-07 14:56:49 +0530

    Added Coments

--- examples/truthordare.py
+++ examples/truthordare.py
@@ -2,12 +2,18 @@ import dhravyapy
 import asyncio
 
 async def main():
+    # Assign a variable for the truth or dare
     tod = dhravyapy.Fun.truthordare()
+    # Takes the truth property of the tod variable
     truth = tod.truth
+    # Takes the dare property of the tod variable
     dare = tod.dare
+    # Prints the dare
     print(f"If you want dare... here is a dare : {dare}")
+    # Prints the truth
     print(f"If you want a truth... here is a truth : {truth}")
 
 if __name__ == __main__:
+    # Creates and runs the async loop synchronously
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
